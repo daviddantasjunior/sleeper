bf3156c95dcb14ad5aeb6db9f46fb5d435ef9da9
David Dantas <daviddantasjunior@gmail.com>
2023-06-25 21:28:11 -0300

    feat: reservation CRUD

---
+++ apps/reservations/src/reservations.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+import { ReservationsRepository } from './reservations.repository';
+
+describe('ReservationsController', () => {
+  let controller: ReservationsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ReservationsController],
+      providers: [ReservationsService, ReservationsRepository],
+    }).compile();
+
+    controller = module.get<ReservationsController>(ReservationsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ apps/reservations/src/reservations.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ReservationsService } from './reservations.service';
+import { ReservationsRepository } from './reservations.repository';
+
+describe('ReservationsService', () => {
+  let service: ReservationsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ReservationsService, ReservationsRepository],
+    }).compile();
+
+    service = module.get<ReservationsService>(ReservationsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
